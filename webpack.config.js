df2a5af094b6e0c14006e7f3ad568e300e822c17
Valentin Vasile <75018284+valvas29@users.noreply.github.com>
2022-08-30 12:25:36 +0200

    moin Julian und Leo, hier als hilfe:

--- webpack.config.js
+++ webpack.config.js
@@ -24,44 +24,47 @@ for (let exercise of [
     "quaternion",
     "project"
 ]) {
-    if (fs.existsSync(path.resolve(__dirname, `src/${exercise}-boilerplate.ts`))) {
-        entries[exercise] = `./src/${exercise}-boilerplate.ts`;
+    if (
+      fs.existsSync(path.resolve(__dirname, `src/${exercise}-boilerplate.ts`))
+    ) {
+      entries[exercise] = `./src/${exercise}-boilerplate.ts`;
     }
-}
-
-module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
+  }
+  
+  module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
     entry: entries,
     output: {
-        filename: '[name].bundle.js',
-        publicPath: "/interactive-computer-graphics/",
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     devServer: {
-        contentBase: path.join(__dirname, 'dist')
+      static: path.join(__dirname, "dist"),
     },
     module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.glsl$/i,
-                use: 'raw-loader'
-            }
-        ]
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.glsl$/i,
+          use: "raw-loader",
+        },
+      ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+      extensions: [".tsx", ".ts", ".js"],
     },
-};
+  };
+  
